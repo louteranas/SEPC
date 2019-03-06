f62518cac360034a2ec7d4c6a94d80c585a8a083
Anas Louter <anas.louter@ensimag.grenoble-inp.fr>
2019-03-06 12:15:43 +0100

    added files

---
+++ TP1/malloc/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /user/2/caulierg/SEPC/TP1/ensimag-malloc
+# Build directory: /user/2/caulierg/SEPC/TP1/ensimag-malloc/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(AllTestsAllocator "alloctest")
