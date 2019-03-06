de44a1319e696ea552d955da30941fc8c451a8bb
Anas Louter <anas.louter@ensimag.grenoble-inp.fr>
2019-03-06 12:46:16 +0100

    dir name

--- TP1/malloc/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /user/2/caulierg/SEPC/TP1/ensimag-malloc
-# Build directory: /user/2/caulierg/SEPC/TP1/ensimag-malloc/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(AllTestsAllocator "alloctest")
--- TP1/malloc/tests/README.md
+++
@@ -1,6 +0,0 @@
-Tests unitaires
-==========
-
-Ces tests sont réalisés en utilisant la bibliothèque libgtest (Google
-test unit).
-
