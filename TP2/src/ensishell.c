c9563b3e1e7f62670382a2af48ce744ad0188edf
Guillaume Caulier <Guillaume.Caulier@grenoble-inp.org>
2019-04-01 16:29:28 +0200

    3 premieres questions

--- TP2/src/ensishell.c
+++ TP2/src/ensishell.c
@@ -8,8 +8,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "variante.h"
+#include "ensishell.h"
 #include "readcmd.h"
 
 #ifndef VARIANTE
@@ -25,6 +29,91 @@
 #if USE_GUILE == 1
 #include <libguile.h>
 
+void q1() {
+	while (1) {
+		struct cmdline *l;
+		char *line=0;
+		int i, j;
+		char *prompt = "ensishell>";
+
+		/* Readline use some internal memory structure that
+			 can not be cleaned at the end of the program. Thus
+			 one memory leak per command seems unavoidable yet */
+		line = readline(prompt);
+		if (line == 0 || ! strncmp(line,"exit", 4)) {
+			terminate(line);
+		}
+
+	#if USE_GNU_READLINE == 1
+		add_history(line);
+	#endif
+
+
+	#if USE_GUILE == 1
+		/* The line is a scheme command */
+		if (line[0] == '(') {
+			char catchligne[strlen(line) + 256];
+			sprintf(catchligne, "(catch #t (lambda () %s) (lambda (key . parameters) (display \"mauvaise expression/bug en scheme\n\")))", line);
+			scm_eval_string(scm_from_locale_string(catchligne));
+			free(line);
+												continue;
+								}
+	#endif
+
+	/* parsecmd free line and set it up to 0 */
+	l = parsecmd( & line);
+
+	/* If input stream closed, normal termination */
+	if (!l) {
+
+		terminate(0);
+	}
+
+	if (l->err) {
+		/* Syntax error, read another command */
+		printf("error: %s\n", l->err);
+		continue;
+	}
+
+	if (l->in) printf("in: %s\n", l->in);
+	if (l->out) printf("out: %s\n", l->out);
+	if (l->bg) printf("background (&)\n");
+
+	/* Display each command of the pipe */
+	for (i=0; l->seq[i]!=0; i++) {
+		char **cmd = l->seq[i];
+		printf("seq[%d]: ", i);
+											for (j=0; cmd[j]!=0; j++) {
+															printf("'%s' ", cmd[j]);
+											}
+		printf("\n");
+	}
+	pid_t pid;
+
+	for (i=0; l->seq[i]!=0; i++) {
+			char **cmd = l->seq[i];
+			switch(pid = fork()) {
+				case -1:
+					perror("erroooooor"); break;
+				case 0:
+				{
+					//printf("ta mere\n");
+					execvp(*cmd, cmd);
+					break;
+				}
+				default:
+				{
+					printf("%d, je suis ton père\n", pid);
+					if(!l->bg)						
+						wait(NULL);
+
+					break;
+				}
+			}
+		}
+	}
+}
+
 int question6_executer(char *line)
 {
 	/* Question 6: Insert your code to execute the command line
@@ -36,7 +125,7 @@ int question6_executer(char *line)
 
 	/* Remove this line when using parsecmd as it will free it */
 	free(line);
-	
+
 	return 0;
 }
 
@@ -68,66 +157,7 @@ int main() {
         scm_c_define_gsubr("executer", 1, 0, 0, executer_wrapper);
 #endif
 
-	while (1) {
-		struct cmdline *l;
-		char *line=0;
-		int i, j;
-		char *prompt = "ensishell>";
-
-		/* Readline use some internal memory structure that
-		   can not be cleaned at the end of the program. Thus
-		   one memory leak per command seems unavoidable yet */
-		line = readline(prompt);
-		if (line == 0 || ! strncmp(line,"exit", 4)) {
-			terminate(line);
-		}
-
-#if USE_GNU_READLINE == 1
-		add_history(line);
-#endif
 
 
-#if USE_GUILE == 1
-		/* The line is a scheme command */
-		if (line[0] == '(') {
-			char catchligne[strlen(line) + 256];
-			sprintf(catchligne, "(catch #t (lambda () %s) (lambda (key . parameters) (display \"mauvaise expression/bug en scheme\n\")))", line);
-			scm_eval_string(scm_from_locale_string(catchligne));
-			free(line);
-                        continue;
-                }
-#endif
-
-		/* parsecmd free line and set it up to 0 */
-		l = parsecmd( & line);
-
-		/* If input stream closed, normal termination */
-		if (!l) {
-		  
-			terminate(0);
-		}
-		
-
-		
-		if (l->err) {
-			/* Syntax error, read another command */
-			printf("error: %s\n", l->err);
-			continue;
-		}
-
-		if (l->in) printf("in: %s\n", l->in);
-		if (l->out) printf("out: %s\n", l->out);
-		if (l->bg) printf("background (&)\n");
-
-		/* Display each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
-			printf("seq[%d]: ", i);
-                        for (j=0; cmd[j]!=0; j++) {
-                                printf("'%s' ", cmd[j]);
-                        }
-			printf("\n");
-		}
-	}
-
+		q1();
 }
---
+++ TP2/src/ensishell.h
@@ -0,0 +1,10 @@
+#ifndef ENSISHELL_H
+#define ENSISHELL_H
+
+void q1();
+
+int question6_executer(char *line);
+
+void terminate(char *line);
+
+#endif
